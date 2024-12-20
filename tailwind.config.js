576f772a4fc3035b80b6023a2d3528f8e1213271
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2024-12-20 01:56:55 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Inclua o arquivo HTML principal se estiver usando Vite
+    "./src/**/*.{js,jsx,ts,tsx}", // Todos os arquivos no diretório src com as extensões listadas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
